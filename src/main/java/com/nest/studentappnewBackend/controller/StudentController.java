f8142ce18efbdc9a237c90ce0483f630ed5f4926
Amal <amalprakash2025@gmail.com>
2022-12-05 14:31:28 +0530

    initial commit

---
+++ src/main/java/com/nest/studentappnewBackend/controller/StudentController.java
@@ -0,0 +1,13 @@
+package com.nest.studentappnewBackend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StudentController {
+
+    @GetMapping("/")
+    public String Hompage(){
+        return  "hello world!";
+    }
+}
