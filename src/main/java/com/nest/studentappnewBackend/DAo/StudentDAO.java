d69c8d898dc6b51b0912f57bdd6bd508db3db585
Amal <amalprakash2025@gmail.com>
2022-12-08 15:49:34 +0530

    added search, delete

--- src/main/java/com/nest/studentappnewBackend/DAo/StudentDAO.java
+++ src/main/java/com/nest/studentappnewBackend/DAo/StudentDAO.java
@@ -1,7 +1,21 @@
 package com.nest.studentappnewBackend.DAo;
 
 import com.nest.studentappnewBackend.Model.Students;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface StudentDAO extends CrudRepository <Students, Integer> {
+
+    @Query(value = "SELECT `id`, `admission_no`, `college`, `rollno`, `name` FROM `students` WHERE `rollno`=:rollNO",nativeQuery = true)
+    List<Students> Search(@Param("rollNO") Integer rollNO);
+
+    @Transactional
+    @Modifying
+    @Query(value = "DELETE FROM `students` WHERE `rollno`=:rollNO",nativeQuery = true)
+    void Delete(@Param("rollNO")Integer rollNO);
 }
--- src/main/java/com/nest/studentappnewBackend/controller/StudentController.java
+++ src/main/java/com/nest/studentappnewBackend/controller/StudentController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.List;
 
 @RestController
@@ -20,13 +21,28 @@ public class StudentController {
     }
 
     @PostMapping( path = "/addStudents", consumes = "application/json", produces = "application/json")
-    public String AddStudents(@RequestBody Students s){
+    public HashMap<String, String> AddStudents(@RequestBody Students s){
         System.out.println(s.getName().toString());
-        return "Add Student";
+        dao.save(s);
+        HashMap<String, String> map = new HashMap<>();
+        map.put("status","success");
+        return map;
     }
 
     @GetMapping("/viewStudents")
     public List<Students> ViewStudents(){
         return (List<Students>) dao.findAll();
+    }
+
+    @PostMapping(path = "/search", consumes = "application/json", produces = "application/json")
+    public List<Students> Search(@RequestBody Students s){
+        return (List<Students>) dao.Search(s.getRollNO());
+    }
+    @PostMapping(path = "/delete", consumes = "application/json", produces = "application/json")
+    public HashMap<String, String> Delete(@RequestBody Students s){
+    dao.Delete(s.getRollNO());
+    HashMap<String,String> map = new HashMap<>();
+    map.put("status","success");
+    return map;
     }
 }
